8a395d5188712c8f4d0bc8f02be4fb41f85445ff
Sonu00100 <sonu89gamer@gmail.com>
2024-12-04 18:51:10 +0530

    Asign Scrollable page2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from "./components/navbar/Header"
 import Home from './components/pages/Home';
 import About from './components/pages/About';
